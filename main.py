d23075a9ef47f1ba86b8d0d17a35cb534fc8d7bf
rent-strike-data <rent-strike-data@protonmail.com>
2020-04-20 08:57:42 -0700

    before selenium bkground chg

--- main.py
+++ main.py
@@ -9,27 +9,26 @@ property_ids = []
 owners = []
 owner_addresses = []
 
-with open('property_ids2.csv', newline='') as f:
+with open('property_ids3.csv', newline='') as f:
     reader = csv.reader(f)
     p_ids = list(reader)
 
 counter = 0
 for p_id in p_ids:
-    p_id_clean = p_id[0]
-    print(p_id_clean)
+    p_id = p_id[0]
 
-    url = "https://www.portlandmaps.com/detail/property/" + p_id_clean + "_did/"
+    url = "https://www.portlandmaps.com/detail/property/" + p_id + "_did/"
 
     driver = webdriver.Chrome()
     driver.get(url)
-    sleep(randint(2, 10))
+    sleep(randint(3, 7))
     html = driver.page_source
 
     soup = BeautifulSoup(html, 'html.parser')
 
     owner_dt = soup.find('dt', string='Owner')
     if owner_dt is None:
-        print('no owner_dt found')
+        # print('no owner_dt found')
         owner = "NULL"
         owner_address = "NULL"
     else:
@@ -40,20 +39,20 @@ for p_id in p_ids:
             owner
         except NameError:
             owner = "NULL"
-        if owner == "NULL":
-            print('no owner found')
-        else:
-            print("owner found")
-            print(owner)
+        # if owner == "NULL":
+        #     # print('no owner found')
+        # else:
+        #     # print("owner found")
+        #     # print(owner)
 
         owner_address = soup.find('dt', string='Owner Address').find_next_sibling('dd').text
         try:
             owner_address
         except NameError:
             owner_address = "NULL"
-        print(owner_address)
+        # print(owner_address)
 
-        property_ids.append(p_id_clean)
+        property_ids.append(p_id)
         owners.append(owner)
         owner_addresses.append(owner_address)
 
@@ -64,14 +63,14 @@ for p_id in p_ids:
         })
 
         # to see your dataframe
-        print(properties)
+        # print(properties)
 
         # write all scraped data to a CSV file every 50 lines
         if counter % 50 == 0:
             properties.to_csv('result_' + str(counter) + '.csv')
 
         counter = counter + 1
-        print('counter = ' + str(counter))
+        print(counter)
         driver.quit()
     # 835k total properties
     # 390k within pdx city limits
