3154377595f6b8bb414533626652a5f51ea28ea0
rent-strike-data <rent-strike-data@protonmail.com>
2020-04-20 17:55:00 -0700

    multiproc working, counter needs fixing

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -2,6 +2,7 @@ import csv
 import requests
 import datetime
 from time import sleep, time
+import pandas as pd
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -9,12 +10,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from bs4 import BeautifulSoup
 
-
-# def get_driver():
-#     driver = webdriver.Chrome()
-#     return driver
+property_ids = []
+owners = []
+owner_addresses = []
 
 def get_driver():
+    # print('get_driver')
     # initialize options
     options = webdriver.ChromeOptions()
     # pass in headless argument to options
@@ -25,15 +26,18 @@ def get_driver():
 
 
 def connect_to_base(browser, p_id):
+    # print('connect_to_base')
+    # print(p_id)
     base_url = f'https://www.portlandmaps.com/detail/property/{p_id}_did/'
+    print(base_url)
     connection_attempts = 0
     while connection_attempts < 3:
         try:
             browser.get(base_url)
-            # wait for table element with id = 'hnmain' to load
+            # wait for table element with id = 'summary' to load
             # before returning True
             WebDriverWait(browser, 5).until(
-                EC.presence_of_element_located((By.ID, 'hnmain'))
+                EC.presence_of_element_located((By.ID, 'summary'))
             )
             return True
         except Exception as ex:
@@ -42,56 +46,44 @@ def connect_to_base(browser, p_id):
             print(f'Attempt #{connection_attempts}.')
     return False
 
-
-def parse_html(html):
+def get_owner_data(html, p_id):
+    # print('get_owner_data')
     # create soup object
     soup = BeautifulSoup(html, 'html.parser')
-    output_list = []
-    # parse soup object to get article id, rank, score, and title
-    tr_blocks = soup.find_all('tr', class_='athing')
-    article = 0
-    for tr in tr_blocks:
-        article_id = tr.get('id')
-        article_url = tr.find_all('a')[1]['href']
-        # check if article is a hacker news article
-        if 'item?id=' in article_url:
-            article_url = f'https://news.ycombinator.com/{article_url}'
-        load_time = get_load_time(article_url)
+    owner_dt = soup.find('dt', string='Owner')
+    if owner_dt is None:
+        # print('no owner_dt found')
+        owner = "NULL"
+        owner_address = "NULL"
+    else:
+        # print("owner_dt found")
+        # print(owner_dt)
+        owner = owner_dt.find_next_sibling('dd').text
         try:
-            score = soup.find(id=f'score_{article_id}').string
-        except Exception as ex:
-            score = '0 points'
-        article_info = {
-            'id': article_id,
-            'load_time': load_time,
-            'rank': tr.span.string,
-            'score': score,
-            'title': tr.find(class_='storylink').string,
-            'url': article_url
-        }
-        # appends article_info to output_list
-        output_list.append(article_info)
-        article += 1
-    return output_list
+            owner
+        except NameError:
+            owner = "NULL"
+        # if owner == "NULL":
+        #     # print('no owner found')
+        # else:
+        #     # print("owner found")
+        #     # print(owner)
 
+        owner_address = soup.find('dt', string='Owner Address').find_next_sibling('dd').text
+        try:
+            owner_address
+        except NameError:
+            owner_address = "NULL"
+        # print(owner_address)
 
-def get_load_time(article_url):
-    try:
-        # set headers
-        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        # make get request to article_url
-        response = requests.get(
-            article_url, headers=headers, stream=True, timeout=3.000)
-        # get page load time
-        load_time = response.elapsed.total_seconds()
-    except Exception as ex:
-        load_time = 'Loading Error'
-    return load_time
+        property_ids.append(p_id)
+        owners.append(owner)
+        owner_addresses.append(owner_address)
 
+        properties = pd.DataFrame({
+            'p_id': property_ids,
+            'owner': owners,
+            'owner_address': owner_addresses
+        })
 
-def write_to_file(output_list, filename):
-    for row in output_list:
-        with open(filename, 'a') as csvfile:
-            fieldnames = ['id', 'load_time', 'rank', 'score', 'title', 'url']
-            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow(row)
+        return properties
--- script.py
+++ script.py
@@ -1,84 +1,65 @@
 import datetime
+from itertools import repeat
 from time import sleep, time
+from multiprocessing import Pool, cpu_count
+import csv
 
 from scrapers.scraper import get_driver, connect_to_base, \
-    parse_html, write_to_file
+    get_owner_data
 
 
-def run_process(p_id, counter, browser):
+def run_process(counter, output_filename):
+    print(f'run_process: {counter}')
+    browser = get_driver()
+    p_id = get_pid(counter)
     if connect_to_base(browser, p_id):
         sleep(2)
         html = browser.page_source
-        soup = parse_html(html)
-        properties = get_owner_data(soup, p_id)
-        print(properties)
+        properties = get_owner_data(html, p_id)
+        # print(properties)
         if counter % 50 == 0:
-            properties.to_csv('result_' + str(counter) + '.csv')
-        print(counter)
+            print(f'writing to csv: {output_filename}')
+            properties.to_csv('result_' + output_filename + '.csv')
+        counter = counter + 1
+        browser.quit()
     else:
         print('Error connecting')
+        browser.quit()
 
 def get_pid(counter):
+    print(f'get_pid {counter}')
     with open("property_ids3.csv", newline='') as f:
-        r = csv.reader(f):
+        r = csv.reader(f)
         for i in range(counter):  # count from 0 to counter
             next(r)  # discard intervening rows
         row = next(r)
-        print(f'row: {row}')
-        return row
-
-def get_owner_data(soup, p_id):
-    owner_dt = soup.find('dt', string='Owner')
-    if owner_dt is None:
-        # print('no owner_dt found')
-        owner = "NULL"
-        owner_address = "NULL"
-    else:
-        # print("owner_dt found")
-        # print(owner_dt)
-        owner = owner_dt.find_next_sibling('dd').text
-        try:
-            owner
-        except NameError:
-            owner = "NULL"
-        # if owner == "NULL":
-        #     # print('no owner found')
-        # else:
-        #     # print("owner found")
-        #     # print(owner)
-
-        owner_address = soup.find('dt', string='Owner Address').find_next_sibling('dd').text
-        try:
-            owner_address
-        except NameError:
-            owner_address = "NULL"
-        # print(owner_address)
-
-        property_ids.append(p_id)
-        owners.append(owner)
-        owner_addresses.append(owner_address)
-
-        properties = pd.DataFrame({
-            'p_id': property_ids,
-            'owner': owners,
-            'owner_address': owner_addresses
-        })
-
-        return properties
+        # print(f'row: {row}')
+        return row[0]
 
 
 if __name__ == '__main__':
+    print(f'cpu_count - 1 = {cpu_count() -1}')
     start_time = time()
     counter = 1
     output_timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
     output_filename = f'output_{output_timestamp}.csv'
-    browser = get_driver()
-    while counter <= 50:
+
+    # browser = get_driver()
+    # while counter <= 50:
+    #     p_id = get_pid(counter)
+    #     print(f'Scraping property #{counter}, p_id {p_id}...')
+    #     run_process(p_id, counter, output_filename, browser)
+    #     counter = counter + 1
+    # browser.quit()
+
+    with Pool(cpu_count() - 1) as p:
         p_id = get_pid(counter)
-        print(f'Scraping property #{counter}, p_id {p_id}...')
-        run_process(p_id, counter, browser)
-        counter = counter + 1
-    browser.quit()
+        p.starmap(run_process, zip(range(1, 51), repeat(output_filename)))
+    p.close()
+    p.join()
+
     end_time = time()
     elapsed_time = end_time - start_time
     print(f'Elapsed run time: {elapsed_time} seconds')
+
+
